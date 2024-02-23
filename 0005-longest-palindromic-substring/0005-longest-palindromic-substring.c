c837f83eb948279979500c5f67a9be6b0303e20c
FlamurMaliqi <120040061+FlamurMaliqi@users.noreply.github.com>
2024-02-23 23:19:48 +0100

    Time: 91 ms (35.85%), Space: 10.6 MB (8.88%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+    if (n <= 1) return s;
+
+    int dp[n][n];
+    memset(dp, 0, sizeof(dp));
+
+    int start = 0;
+    int maxLen = 1; 
+
+    for (int i = 0; i < n; ++i)
+        dp[i][i] = 1;
+
+    for (int i = 0; i < n - 1; ++i) {
+        if (s[i] == s[i + 1]) {
+            dp[i][i + 1] = 1;
+            start = i;
+            maxLen = 2;
+        }
+    }
+
+    for (int len = 3; len <= n; ++len) {
+        for (int i = 0; i < n - len + 1; ++i) {
+            int j = i + len - 1; 
+            if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                dp[i][j] = 1;
+                start = i;
+                maxLen = len;
+            }
+        }
+    }
+
+    char* result = (char*)malloc((maxLen + 1) * sizeof(char));
+    strncpy(result, s + start, maxLen);
+    result[maxLen] = '\0'; 
+    return result;
+}
